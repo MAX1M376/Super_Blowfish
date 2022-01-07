f082be2935241a9bad12091acf8b73067881bcbc
MADcampusgithub <maxime.adler@campus-la-chataigneraie.org>
2022-01-07 14:45:57 +0100

    Water animation

---
+++ Assets/OnWater.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnWater : MonoBehaviour
+{
+    [SerializeField]
+    [Header("Object to detect :")]
+    private new GameObject gameObject;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == gameObject.name)
+        {
+            collision.gameObject.GetComponent<PlayerBehaviour>().isOnWater = true;
+            var velo = collision.gameObject.GetComponent<Rigidbody2D>().velocity;
+            velo.y = 0;
+            collision.gameObject.GetComponent<Rigidbody2D>().velocity = velo;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == gameObject.name)
+        {
+            collision.gameObject.GetComponent<PlayerBehaviour>().isOnWater = false;
+        }
+    }
+}
--- Assets/Scipts/PlayerBehaviour.cs
+++ Assets/Scipts/PlayerBehaviour.cs
@@ -22,12 +22,24 @@ public class PlayerBehaviour : MonoBehaviour
     [SerializeField]
     private float airResistence = 0.01f;
 
+    [SerializeField]
+    private float gravityForceOnWater = 14f;
+
+    [SerializeField]
+    private float airResistenceOnWater = 0.01f;
+
     [Header("Movement :")]
     [SerializeField]
     private float jumpForce = 10f;
 
     [SerializeField]
     private float timeBetweenJump = 0.5f;
+
+    [SerializeField]
+    private float jumpForceOnWater = 10f;
+
+    [SerializeField]
+    private float timeBetweenJumpOnWater = 0.5f;
 
     [SerializeField]
     [Range(0.1f, 3f)]
@@ -42,10 +54,12 @@ public class PlayerBehaviour : MonoBehaviour
 
     [Header("Map :")]
     [SerializeField]
-    private Collider2D mapCollider;
+    private string mapName;
 
     [SerializeField]
     private float offsetGroundDistance = 0.02f;
+
+    public bool isOnWater = false;
 
     private void Start()
     {
@@ -58,10 +72,10 @@ public class PlayerBehaviour : MonoBehaviour
         // Collision avec le sols
         RaycastHit2D[] downHits = Physics2D.RaycastAll(transform.position, Vector2.down, (capc.size.y / 2f) + offsetGroundDistance);
         
-        if (!downHits.Any(x => x.collider.gameObject.name == mapCollider.gameObject.name)) // Si pas de collision alors
+        if (!downHits.Any(x => x.collider.gameObject.name == mapName)) // Si pas de collision alors
         { 
             // Appliquation gravité
-            force.y -= gravityForce * Time.deltaTime;
+            force.y -= (isOnWater ? gravityForceOnWater : gravityForce) * Time.deltaTime;
         }
         else
         {
@@ -75,34 +89,14 @@ public class PlayerBehaviour : MonoBehaviour
             rotation = Quaternion.Euler(0, 0, 0);
         }
 
+        Movement(isOnWater ? timeBetweenJumpOnWater : timeBetweenJump, isOnWater ? jumpForceOnWater : jumpForce);
+
         // Collision avec le plafond
         RaycastHit2D[] upHits = Physics2D.RaycastAll(transform.position, Vector2.up, (capc.size.y / 2f) + offsetGroundDistance);
-        if (upHits.Any(x => x.collider.gameObject.name == mapCollider.gameObject.name)) // Si collision avec le plafond
+        if (upHits.Any(x => x.collider.gameObject.name == mapName)) // Si collision avec le plafond
         {
             // Annulation des forces vertical
             force.y = -1f;
-        }
-
-        // Les inputs
-        if (Input.touchCount == 1 && Time.time - lastJump >= timeBetweenJump)
-        {
-            // Recupération de la position du touché
-            var mousePos2D = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
-            var pos2D = new Vector2(transform.position.x, transform.position.y);
-
-            // Calcul de la direction et de la force du saut
-            var direction = (mousePos2D - pos2D).normalized;
-            var jump = Vector2.Distance(mousePos2D, pos2D) >= distancePowerfullJump ? (jumpForce * jumpMaxForce) : jumpForce;
-            
-            // Application des force et reset du jump time
-            force = direction * jump;
-            lastJump = Time.time;
-        }
-
-        // Reduction le temps entre chaque si spam clic
-        if (Input.touchCount == 0 && Time.time - lastJump <= timeBetweenJump)
-        {
-            lastJump = 0f;
         }
 
         // Tourne le sprite dans le direction
@@ -118,12 +112,37 @@ public class PlayerBehaviour : MonoBehaviour
     private void FixedUpdate()
     {
         // Application de la résistance de l'air
-        force.x = Mathf.SmoothDamp(force.x, 0f, ref horizontalVelocity, 300f * (1 - airResistence) * Time.fixedDeltaTime);
+        force.x = Mathf.SmoothDamp(force.x, 0f, ref horizontalVelocity, 300f * (1 - (isOnWater ? airResistenceOnWater : airResistence)) * Time.fixedDeltaTime);
 
         // Application des toutes les forces sur le rigidbody
         rb.velocity = Vector2.SmoothDamp(rb.velocity, force, ref velocity, Time.fixedDeltaTime);
 
         // Application de la rotation du sprite en fnction de la direction
         transform.GetChild(0).transform.localRotation = Quaternion.Slerp(transform.GetChild(0).transform.localRotation, rotation, Time.fixedDeltaTime * rotationSpeed);
+    }
+
+    private void Movement(float timeJump, float forceJump)
+    {
+        // Les inputs
+        if (Input.touchCount == 1 && Time.time - lastJump >= timeJump)
+        {
+            // Recupération de la position du touché
+            var mousePos2D = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+            var pos2D = new Vector2(transform.position.x, transform.position.y);
+
+            // Calcul de la direction et de la force du saut
+            var direction = (mousePos2D - pos2D).normalized;
+            var jump = Vector2.Distance(mousePos2D, pos2D) >= distancePowerfullJump ? (forceJump * jumpMaxForce) : forceJump;
+
+            // Application des force et reset du jump time
+            force = direction * jump;
+            lastJump = Time.time;
+        }
+
+        // Reduction le temps entre chaque si spam clic
+        if (Input.touchCount == 0 && Time.time - lastJump <= timeJump)
+        {
+            lastJump = 0f;
+        }
     }
 }
