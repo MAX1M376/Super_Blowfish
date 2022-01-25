16af9862cd76d448ee3951de90d3d370d8abfbe2
MADcampusgithub <maxime.adler@campus-la-chataigneraie.org>
2022-01-25 15:11:59 +0100

    stock prizes in json file

--- Assets/Models/Prize.cs
+++ Assets/Models/Prize.cs
@@ -8,5 +8,54 @@ public class Prize
 {
     public string Name { get; set; }
     public string Description { get; set; }
+    public Sprite Image { get; set; }
+}
+
+public class PrizeString
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
     public string Image { get; set; }
+
+    public Prize ToPrize()
+    {
+        Sprite image;
+
+        try
+        {
+            image = Resources.Load<Sprite>(Image);
+        }
+        catch (Exception)
+        {
+            image = null;
+        }
+
+        return new Prize
+        {
+            Name = Name,
+            Description = Description,
+            Image = image
+        };
+    }
+
+    public Prize ToPrize(Sprite defaultSprite)
+    {
+        Sprite image;
+
+        try
+        {
+            image = Resources.Load<Sprite>(Image);
+        }
+        catch (Exception)
+        {
+            image = defaultSprite;
+        }
+
+        return new Prize
+        {
+            Name = Name,
+            Description = Description,
+            Image = image
+        };
+    }
 }
---
+++ Assets/Scipts/Player/InventoryScript.cs
@@ -0,0 +1,33 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+public class InventoryScript : MonoBehaviour
+{
+    [HideInInspector] public List<Prize> AllPrizes { get; private set; }
+    public List<Prize> Inventory { get; set; }
+
+    [Header("Property")]
+    [SerializeField] private Sprite defaultSprite;
+
+    void Start()
+    {
+        try
+        {
+            List<PrizeString> prizesString;
+            using (var reader = new StreamReader(@"Assets\prizes.json"))
+            {
+                prizesString = JsonConvert.DeserializeObject<List<PrizeString>>(reader.ReadToEnd());
+            }
+            AllPrizes = prizesString.Select(x => x.ToPrize()).ToList();
+        }
+        catch (Exception ex)
+        {
+            Debug.LogException(ex);
+        }
+    }
+}
